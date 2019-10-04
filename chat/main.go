484d49de3d6c606df8dab672080c8c3c80c2a38c
masataka_iMac <cdehilmnrr@gmail.com>
2019-10-05 07:57:26 +0900

    WebSocketを使った簡易チャットアプリ

---
+++ chat/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+// templは1つのテンプレートを表します
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ =
+			template.Must(template.ParseFiles(filepath.Join("templates",
+				t.filename)))
+	})
+	_ = t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8888", "アプリケーションのアドレス")
+	flag.Parse()
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	go r.run()
+	// Webサーバーを開始します
+	log.Println("Webサーバーを開始します。ポート：", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module go_chat_oreilly
+
+go 1.12
+
+require github.com/gorilla/websocket v1.4.1 // indirect
