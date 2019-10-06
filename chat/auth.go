4d9813dfa0cf91e2c01d958da212a5cb87e454ac
masataka_iMac <cdehilmnrr@gmail.com>
2019-10-06 15:34:37 +0900

    ログアウト機能を追加

--- chat/auth.go
+++ chat/auth.go
@@ -16,7 +16,7 @@ type authHandler struct {
 }
 
 func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if _, err := r.Cookie("auth"); err == http.ErrNoCookie {
+	if cookie, err := r.Cookie("auth"); err == http.ErrNoCookie || cookie.Value == "" {
 		w.Header().Set("Location", "/login")
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	} else if err != nil {
