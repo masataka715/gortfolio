08e3005081b023728492755f11e626b0b3bf97a6
masataka_iMac <cdehilmnrr@gmail.com>
2019-10-10 22:31:57 +0900

    画像のアップロード・表示

--- chat/main.go
+++ chat/main.go
@@ -68,6 +68,9 @@ func main() {
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	})
 	http.Handle("/upload", &templateHandler{filename: "upload.html"})
+	http.HandleFunc("/uploader", uploaderHandler)
+	http.Handle("/avatars/",
+		http.StripPrefix("/avatars/", http.FileServer(http.Dir("./avatars"))))
 	http.Handle("/room", r)
 	go r.run()
 	// Webサーバーを開始します
