419ebd4fe4dc657c9dffafb118b18267b1e3077d
masataka_iMac <cdehilmnrr@gmail.com>
2019-12-30 17:57:22 +0900

    lambda and sns test by go

--- lambda/hello.go
+++ lambda/hello.go
@@ -3,31 +3,30 @@ package main
 import (
 	"log"
 
-	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/sns"
 )
 
-type MyEvent struct {
-	BucketName string `json:"bucket_name"`
-}
-
-func hello(event MyEvent) {
-	// NewStaticCredentialsの第３引数はtoken
-	creds := credentials.NewStaticCredentials("ACCESS_KEY", "SECRET_ACCESS_KEY", "")
-	session, _ := session.NewSession(&aws.Config{
-		Credentials: creds,
-		Region:      aws.String("ap-northeast-1")},
-	)
-	svc := s3.New(session)
-	resp, _ := svc.CreateBucket(&s3.CreateBucketInput{
-		Bucket: aws.String(event.BucketName),
-	})
-	log.Println(resp) //ここの結果がテストの実行結果のログ出力やCloudWatchのロググループで確認できる
+func hello() {
+	// ~/.aws/credentialsからアクセスキーを読み込む
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	svc := sns.New(sess)
+	result, _ := svc.ListTopics(nil)
+	for _, t := range result.Topics {
+		log.Println(*t.TopicArn)
+		input := &sns.PublishInput{
+			Message:  aws.String("Hello world!"),
+			TopicArn: aws.String(*t.TopicArn),
+		}
+		result, _ := svc.Publish(input)
+		log.Println(result)
+	}
 }
 
 func main() {
-	lambda.Start(hello)
+	// lambda.Start(hello)
+	hello()
 }
